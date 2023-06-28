06f90c84f5306a2f2ece90169e9d7d84f32d945e
isha_nepcoms <isha.niraula57@gmail.com>
2023-06-28 18:53:42 +0545

    blogs

--- resources/views/client/index.blade.php
+++ resources/views/client/index.blade.php
@@ -452,7 +452,7 @@
             <div class="row">
                 <div class="col-lg-6">
                     <img src="https://images.unsplash.com/photo-1618220179428-22790b461013?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8aW50ZXJpb3IlMjBkZXNpZ258ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&w=500&q=60"
-                        alt="contact image" class="w-100 h-100">
+                        alt="contact image" class="contact-image">
                 </div>
                 <div class="col-lg-6">
                     <form method="POST" action="{{ route('contact.store') }}">
