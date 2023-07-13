65da759f6b736b0e20151780222c61eaaa9bacd7
isha_nepcoms <isha.niraula57@gmail.com>
2023-07-13 10:44:54 +0545

    map updated

--- resources/views/client/layouts/footer.blade.php
+++ resources/views/client/layouts/footer.blade.php
@@ -44,7 +44,7 @@
                     <div class="widget">
                         <h4>Location</h4>
                         <div class="map">
-                            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d56516.31713617815!2d85.2849330560132!3d27.708954252218156!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x39eb198a307baabf%3A0xb5137c1bf18db1ea!2sKathmandu%2044600!5e0!3m2!1sen!2snp!4v1688533698138!5m2!1sen!2snp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+                            <iframe src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3532.97192842128!2d85.35627931506164!3d27.687262482800325!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMjfCsDQxJzE0LjIiTiA4NcKwMjEnMzAuNSJF!5e0!3m2!1sen!2snp!4v1689224295474!5m2!1sen!2snp" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
                         </div>
                     </div>
                 </div>
