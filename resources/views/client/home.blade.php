5e5945adc050d977d3cdee5c03b24487cd89e4db
isha_nepcoms <isha.niraula57@gmail.com>
2023-07-25 17:17:16 +0545

    gallery update

--- resources/views/client/home.blade.php
+++ resources/views/client/home.blade.php
@@ -334,8 +334,7 @@
                     <!--//==Facts Counter Item==//-->
                     <div class="buttons">
                         <div class="video">
-                            <button type="button" class="btn btn-primary" data-toggle="modal"
-                                data-target="#myModal">
+                            <button type="button" class="btn btn-primary" id="myVideoBtn">
                                 <i class="far fa-play-circle"></i>
                                 Play Video
 
@@ -347,23 +346,7 @@
 
         </div>
     </section>
-    <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
-              <h4 class="modal-title" id="myModalLabel">Modal title</h4>
-            </div>
-            <div class="modal-body">
-              ...
-            </div>
-            <div class="modal-footer">
-              <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-            
-            </div>
-          </div>
-        </div>
-      </div>
+
     <section class="faqs padB100">
         <div class="container">
             <div class="row">
@@ -434,10 +417,49 @@
             </div>
         </div>
     </section>
+
+    <div id="myModal" class="modal">
+        <!-- Modal content -->
+        <div class="modal-content">
+            <span class="close">&times;</span>
+            <video width="93%" height="600" style="object-fit: cover; z-index:9999;" controls>
+                <source src="assets/img/video-1.mp4" type="video/mp4">
+              Your browser does not support the video tag.
+              </video>
+        </div>
+
+    </div>
 @endsection
 
 @push('scripts')
     <script type="text/javascript" src="https://code.jquery.com/jquery-1.12.0.min.js"></script>
+    <script>
+        // Get the modal
+        var modal = document.getElementById("myModal");
+
+        // Get the button that opens the modal
+        var btn = document.getElementById("myVideoBtn");
+
+        // Get the <span> element that closes the modal
+        var span = document.getElementsByClassName("close")[0];
+
+        // When the user clicks the button, open the modal 
+        btn.onclick = function() {
+            modal.style.display = "block";
+        }
+
+        // When the user clicks on <span> (x), close the modal
+        span.onclick = function() {
+            modal.style.display = "none";
+        }
+
+        // When the user clicks anywhere outside of the modal, close it
+        window.onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
+        }
+    </script>
     <script>
         $(document).ready(function() {
             $('.counter-value').each(function() {
