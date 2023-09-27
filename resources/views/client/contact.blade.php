506ef27563ebe5624972185210bdef64b11c3280
isha_nepcoms <isha.niraula57@gmail.com>
2023-09-27 10:15:23 +0545

    link

--- resources/views/client/contact.blade.php
+++ resources/views/client/contact.blade.php
@@ -39,11 +39,11 @@
                     <div class="social-bar">
                         <ul>
                             <li>
-                                <a href="https://www.facebook.com/"> <i class="fab fa-facebook-f"></i></a>
+                                <a href="https://www.facebook.com/profile.php?id=100093427325606"> <i class="fab fa-facebook-f"></i></a>
                             </li>
-                            <li><a href="https://www.twitter.com/"><i class="fab fa-twitter"></i></a></li>
-                            <li><a href="https://www.instagram.com/"><i class="fab fa-instagram"></i></a></li>
-                            <li><a href="https://www.linkdin.com/"><i class="fab fa-linkedin-in"></i></a></li>
+                            <li><a href="https://www.facebook.com/profile.php?id=100093427325606"><i class="fab fa-twitter"></i></a></li>
+                            <li><a href="https://www.facebook.com/profile.php?id=100093427325606"><i class="fab fa-instagram"></i></a></li>
+                            <li><a href="https://www.facebook.com/profile.php?id=100093427325606"><i class="fab fa-linkedin-in"></i></a></li>
                         </ul>
                     </div>
                 </div>
